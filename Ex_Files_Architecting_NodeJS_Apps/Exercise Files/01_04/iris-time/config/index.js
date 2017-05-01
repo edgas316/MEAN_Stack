4cd5fc3c93ec448496456ae21b721bb81f9cfd7d
Edvin Gasparian <egasparian@egasparian-macbookpro3.roam.corp.google.com>
2017-05-01 10:31:36 -0700

    resoloving git

--- Ex_Files_Architecting_NodeJS_Apps/Exercise Files/01_04/iris-time/config/index.js
+++
@@ -1,6 +0,0 @@
-require('dotenv').config();
-
-module.exports = {
-    googleTimeApiKey: process.env.GOOGLE_TIME_API_KEY,
-    googleGeoApiKey: process.env.GOOGLE_GEO_API_KEY
-}
--- Ex_Files_Architecting_NodeJS_Apps/Exercise Files/02_01/iris/config/index.js
+++
@@ -1,6 +0,0 @@
-require('dotenv').config();
-
-module.exports = {
-    witToken: process.env.WIT_TOKEN,
-    slackToken: process.env.SLACK_TOKEN
-}
