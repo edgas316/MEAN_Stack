070e593312a729786d24e7b039fffe661d27bd05
Edvin Gasparian <edwingasparian@Edvins-MacBook-Pro.local>
2017-05-05 11:31:32 -0700

    end of training

--- project/frontend/src/app/index.module.js
+++ project/frontend/src/app/index.module.js
@@ -4,19 +4,30 @@ import { config } from './index.config';
 import { routerConfig } from './index.route';
 import { runBlock } from './index.run';
 import { MainController } from './main/main.controller';
-import { GithubContributorService } from '../app/components/githubContributor/githubContributor.service';
-import { WebDevTecService } from '../app/components/webDevTec/webDevTec.service';
-import { NavbarDirective } from '../app/components/navbar/navbar.directive';
-import { MalarkeyDirective } from '../app/components/malarkey/malarkey.directive';
+import { AuthController } from './auth/auth.controller';
+import { NavbarController } from './components/navbar/navbar.controller';
+import { CompareToDirective } from './directives/compareTo.directive';
 
-angular.module('myMeanFront', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ui.router', 'ui.bootstrap', 'toastr'])
+
+// import { GithubContributorService } from '../app/components/githubContributor/githubContributor.service';
+// import { WebDevTecService } from '../app/components/webDevTec/webDevTec.service';
+import { NavbarDirective } from '../app/components/navbar/navbar.directive';
+// import { MalarkeyDirective } from '../app/components/malarkey/malarkey.directive';
+
+
+angular.module('myMeanFront', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ui.router', 'ui.bootstrap', 'toastr', 'satellizer'])
+
+  .constant('API_URL', 'http://localhost:5000/')
   .constant('malarkey', malarkey)
   .constant('moment', moment)
   .config(config)
   .config(routerConfig)
   .run(runBlock)
-  .service('githubContributor', GithubContributorService)
-  .service('webDevTec', WebDevTecService)
+  // .service('githubContributor', GithubContributorService)
+  // .service('webDevTec', WebDevTecService)
   .controller('MainController', MainController)
+  .controller('AuthController', AuthController)
+  .controller('NavbarController', NavbarController)
   .directive('acmeNavbar', NavbarDirective)
-  .directive('acmeMalarkey', MalarkeyDirective);
+  // .directive('acmeMalarkey', MalarkeyDirective)
+  .directive('compareTo', CompareToDirective);
